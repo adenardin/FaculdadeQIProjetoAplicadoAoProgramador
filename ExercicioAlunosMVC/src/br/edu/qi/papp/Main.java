2c17abe9c7b2e98373d39fe98003be3aa775575d
Anderson Denardin <andersondenardin@hotmail.com>
2018-03-27 20:53:47 -0300

    Semana 3 tarefa 3

--- ExercicioAlunosMVC/src/br/edu/qi/papp/Main.java
+++ ExercicioAlunosMVC/src/br/edu/qi/papp/Main.java
@@ -1,17 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.edu.qi.papp;
 
 import br.edu.qi.papp.view.MainView;
+import br.edu.qi.papp.view.Principal;
 
 /**
  *
- * @author Prof Luciano
+ * @author ALUNOS
  */
 public class Main {
-
     public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
         
-        MainView principal = new MainView();
-        principal.mostrar();
+//        MainView principal = new MainView();
+//        principal.mostrar();
     }
-    
 }
